25e361f5581d00aeb7d9cf1a54cc83537f126926
Saurlax <saurlax@qq.com>
2024-10-16 10:36:48 +0800

    feat: add site url and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://man.naosi.org',
 	integrations: [
 		starlight({
 			title: 'DUTManual',
